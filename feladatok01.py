e5ac9b3030b3867e4c8bcb7f45d6e9f4c1bb88ab
Petrik Diak <diak@petrik.hu>
2025-05-20 12:19:21 +0200

    feladat01kesz

--- feladatok01.py
+++ feladatok01.py
@@ -1,2 +1,13 @@
-alma = "alma"
-print("Hello")
+import datetime
+
+VT = "" # vagyottárgy
+VT = input("Mire gyűjt Anna?")
+HK = "" # hánykutya
+HK = int(input("Hány kuytát sétáltatt Anna?"))
+
+print((HK*20)/60 ,"órát sétáltatt")
+print(HK * 700, "Ennyi FT-ot kapott.")
+if HK * 700 <= 5000:
+    print(f"Anna {HK} sétáltatott {(HK*20)/60} óra alatt, ezért {HK*700} forintot kapott. Ez nem elég a {VT}-hez")
+else:
+    print(f"Anna {HK} sétáltatott {(HK*20)/60} óra alatt, ezért {HK*700} forintot kapott. Ez elég a {VT}-hez")
